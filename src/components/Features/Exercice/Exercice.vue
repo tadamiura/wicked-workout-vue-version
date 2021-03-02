673984af2389a757580a52e24450007141bab4c7
Miura <tadamiura@gmail.com>
2021-03-02 14:13:24 +0100

    fetch data of exercice and display it

--- src/components/Features/Exercice/Exercice.vue
+++ src/components/Features/Exercice/Exercice.vue
@@ -1,3 +1,31 @@
 <template>
-    <div>Yolo</div>
+  <div>
+    <h1>Liste des exercices</h1>
+    <ul v-for="exercice in exercices" :key="exercice.id">
+        <li>{{ exercice.name }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            exercices:  {}
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            axios.get('exercices')
+            .then( res => {
+                this.exercices = res.data
+            })
+        }
+    }
+}
+</script>
