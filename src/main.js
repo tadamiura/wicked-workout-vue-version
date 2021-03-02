3dc21bb009bc24a0e61ed82ab8e5af805903daee
Miura <tadamiura@gmail.com>
2021-03-02 14:12:52 +0100

    added default base url

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'https://wicked-workout-back.herokuapp.com/api/'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
