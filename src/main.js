34856107fc758208c94d7982fe10e5d6d40df8f3
Miura <tadamiura@gmail.com>
2021-03-03 10:53:50 +0100

    imported globaly v-video-embed

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import Embed from 'v-video-embed'
 
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'https://wicked-workout-back.herokuapp.com/api/'
 Vue.prototype.$http = axios
+
+Vue.use(Embed);
 
 new Vue({
   router,
