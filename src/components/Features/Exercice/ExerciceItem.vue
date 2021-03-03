69e4a892fdc76c4c2eefd37b97caf9024aaddb7d
Miura <tadamiura@gmail.com>
2021-03-03 10:54:12 +0100

    display video for each exercice

--- src/components/Features/Exercice/ExerciceItem.vue
+++ src/components/Features/Exercice/ExerciceItem.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="w-25 p-3 product-container d-flex flex-column">
+  <div class="w-30 p-3 product-container d-flex flex-column">
     <div>
-      <h3>{{ exercice.name }}</h3>
+      <h5>{{ exercice.name }}</h5>
+      <video-embed class="mx-auto" :src="exercice.url_name"></video-embed>
     </div>
     <button class="btn btn-primary btn-sm float-right">Ajouter à vos favoris</button>
   </div>
