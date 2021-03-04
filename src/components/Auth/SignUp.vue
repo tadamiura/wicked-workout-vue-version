4b41af9e04b51670c4f342cc20bf0bcd800541b1
Miura <tadamiura@gmail.com>
2021-03-04 15:56:49 +0100

    init form

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,5 +1,49 @@
 <template>
-    <div>
-        Sign up
+    <div class="container d-flex flex-column p-3 justify-content-center align-items-center">
+        <h1>Sign In</h1>
+        <form @submit="trySubmit" class="text-left">
+            <div class="form-group">
+                <label>Email</label>
+                <input  v-model="form.email" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label>Username</label>
+                <input  v-model="form.username" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input  v-model="form.password" class="form-control" type="password">
+            </div>
+            <ul v-if="errors.lenght">
+                <li v-for="error in errors" :key="error" class="text-danger">{{ error }}</li>
+            </ul>
+            <button class="btn btn-primary mt-3">SignUp</button>
+        </form>
     </div>
 </template>
+<script>
+export default {
+    name: "SignUp",
+    data() {
+        return {
+            errors: [],
+            form: {
+                email: "",
+                username: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        trySubmit(e) {
+            e.preventDefault()
+            if(this.isValid){
+                console.log(this.form)
+            }
+        },
+        isValid() {
+            return true
+        }
+    }
+}
+</script>
