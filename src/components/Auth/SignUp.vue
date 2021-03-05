93546b73ff1453328fdc3f151c60776601a82903
Miura <tadamiura@gmail.com>
2021-03-05 11:27:46 +0100

    test email validation & handle errors

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,36 +1,43 @@
 <template>
-    <div class="container d-flex flex-column p-3 justify-content-center align-items-center">
-        <h1>Sign Up</h1>
-        <form @submit="trySubmit" class="text-left">
-            <div class="form-group">
-                <label>Email</label>
-                <input  v-model="form.email" class="form-control" type="text">
-            </div>
-            <div class="form-group">
-                <label>Username</label>
-                <input  v-model="form.username" class="form-control" type="text">
-            </div>
-             <div class="form-group">
-                <label>FirstName</label>
-                <input  v-model="form.firstname" class="form-control" type="text">
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input  v-model="form.password" class="form-control" type="password">
-            </div>
-            <ul v-if="errors.lenght">
-                <li v-for="error in errors" :key="error" class="text-danger">{{ error }}</li>
-            </ul>
-            <button class="btn btn-primary mt-3">SignUp</button>
-        </form>
-    </div>
+  <div
+    class="container d-flex flex-column p-3 justify-content-center align-items-center"
+  >
+    <h1>Sign Up</h1>
+    <form @submit="trySubmit" class="text-left">
+      <div class="form-group">
+        <label>Email</label>
+        <input v-model="form.email" class="form-control" type="text" />
+      </div>
+      <div class="form-group">
+        <label>Username</label>
+        <input v-model="form.username" class="form-control" type="text" />
+      </div>
+      <div class="form-group">
+        <label>FirstName</label>
+        <input v-model="form.firstname" class="form-control" type="text" />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input v-model="form.password" class="form-control" type="password" />
+      </div>
+      <ul v-if="errors.lenght">
+        <li v-for="error in formErrors" :key="error" class="text-danger">
+          {{ error }}
+        </li>
+      </ul>
+      <button class="btn btn-primary mt-3" :class="{ disabled: isLoading }">
+        SignUp
+      </button>
+    </form>
+  </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
     name: "SignUp",
     data() {
         return {
-            errors: [],
+            formErrors: [],
             form: {
                 email: "",
                 username: "",
@@ -39,15 +46,28 @@ export default {
             }
         }
     },
+    computed: {
+        ...mapGetters("user", ["isLoading", "errors"])//fetch only isLoading and errors from user
+    },
+    watch: {
+        //in order to observe change of errors and change formError
+        errors(newValue) {
+            this.formErrors = newValue
+        }
+    },
     methods: {
         trySubmit(e) {
             e.preventDefault()
-            if(this.isValid){
+            if(this.isValid && !this.isLoading){
                 this.$store.dispatch("user/trySignUp", this.form)
             }
         },
         isValid() {
-            return true
+            this.formErrors = []
+            if ( /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/.test(this.form.email)) {
+                this.formErrors.push("email non valide");
+            }
+            return this.formErrors.length === 0;
         }
     }
 }
