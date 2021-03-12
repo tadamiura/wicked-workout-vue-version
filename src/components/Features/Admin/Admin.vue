8bab6c0c2a85dab565c0985d26ecb8d52d3497d8
Miura <tadamiura@gmail.com>
2021-03-12 14:38:41 +0100

    created card in Admin as a dashboard of exercice management

--- src/components/Features/Admin/Admin.vue
+++ src/components/Features/Admin/Admin.vue
@@ -1,90 +1,53 @@
 <template>
-  <div
-    class="container d-flex flex-column p-3 justify-content-center align-items-center"
-  >
-    <form @submit="trySubmit" class="text-left">
-      <div class="mb-3 form-group">
-        <label>Nom de l'exercice</label>
-        <input
-          class="form-control"
-          type="text"
-          placeholder="exercice"
-          v-model="form.name"
-          required
-        />
+  <div>
+    <div class="card m-3">
+      <div class="card-header">
+        Ajouter
       </div>
-      <div class="mb-3 form-group">
-        <label>Lien URL de l'exercice</label>
-        <input
-          class="form-control"
-          type="url"
-          placeholder="entrer l'url"
-          v-model="form.url_name"
-        />
+      <div class="card-body">
+        <h5 class="card-title">Pour ajouter un nouvel exercice</h5>
+        <p class="card-text">
+          Accéder au formulaire de création d'un exercice
+        </p>
+        <a
+          v-if="jwtToken && isLoggedIn"
+          href="/admin/exercices/new"
+          class="btn btn-primary"
+          >Aller au formulaire de création</a
+        >
       </div>
-      <div class="my-3 form-check">
-        <input
-          class="form-check-input"
-          type="checkbox" value="is_tabata_workout"
-          v-model="checkedWorkout"
-        />
-        <label class="form-check-label">Tabata</label>
+    </div>
+    <div class="card mb-3 mx-3">
+      <div class="card-header">
+        Modifier
       </div>
-      <div class="mb-3 form-check">
-        <input
-          class="form-check-input"
-          type="checkbox" value="is_six_workout"
-          v-model="checkedWorkout"
-        />
-        <label class="form-check-label">666</label>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
-      <div class="mb-3 form-check">
-        <input
-          class="form-check-input"
-          type="checkbox" value="is_thirty_thirty_workout"
-          v-model="checkedWorkout"
-        />
-        <label class="form-check-label">30/30</label>
+    </div>
+    <div class="card mb-3 mx-3">
+      <div class="card-header">
+        Supprimer
       </div>
-      <button class="btn btn-primary mt-3">Envoyer</button>
-    </form>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import axios from '../../../http'
+import { mapGetters } from "vuex";
 export default {
-  data() {
-    return {
-      form: {
-        name: "",
-        url_name: "",
-        is_six_workout:"0",
-        is_tabata_workout:"0",
-        is_thirty_thirty_workout:"0",
-      },
-      checkedWorkout: []
-    };
-  },
-  methods: {
-    checkboxValues() {
-        if(this.checkedWorkout.includes('is_six_workout')) {
-            this.form.is_six_workout = "1"
-        }
-        if(this.checkedWorkout.includes('is_tabata_workout')) {
-            this.form.is_tabata_workout = "1"
-        }
-        if(this.checkedWorkout.includes('is_thirty_thirty_workout')) {
-            this.form.is_thirty_thirty_workout = "1"
-        }
-    },
-    trySubmit(e) {
-      e.preventDefault(e);
-      this.checkboxValues();
-      axios.post('exercices', this.form)
-      .then( res => res.data)
-      .then(alert(`L'exercice ${this.form.name} a bien été ajouté`))
-      .catch( err => console.log(err))
-    },
+  computed: {
+    ...mapGetters("user", ["isLoggedIn", "jwtToken"]),
   },
 };
 </script>
---
+++ src/components/Features/Admin/AdminAddExercice.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    class="container d-flex flex-column p-3 justify-content-center align-items-center"
+  >
+    <form @submit="trySubmit" class="text-left">
+      <div class="mb-3 form-group">
+        <label>Nom de l'exercice</label>
+        <input
+          class="form-control"
+          type="text"
+          placeholder="exercice"
+          v-model="form.name"
+          required
+        />
+      </div>
+      <div class="mb-3 form-group">
+        <label>Lien URL de l'exercice</label>
+        <input
+          class="form-control"
+          type="url"
+          placeholder="entrer l'url"
+          v-model="form.url_name"
+        />
+      </div>
+      <div class="my-3 form-check">
+        <input
+          class="form-check-input"
+          type="checkbox" value="is_tabata_workout"
+          v-model="checkedWorkout"
+        />
+        <label class="form-check-label">Tabata</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input
+          class="form-check-input"
+          type="checkbox" value="is_six_workout"
+          v-model="checkedWorkout"
+        />
+        <label class="form-check-label">666</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input
+          class="form-check-input"
+          type="checkbox" value="is_thirty_thirty_workout"
+          v-model="checkedWorkout"
+        />
+        <label class="form-check-label">30/30</label>
+      </div>
+      <button class="btn btn-primary mt-3">Envoyer</button>
+    </form>
+  </div>
+</template>
+<script>
+import axios from '../../../http'
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        url_name: "",
+        is_six_workout:"0",
+        is_tabata_workout:"0",
+        is_thirty_thirty_workout:"0",
+      },
+      checkedWorkout: []
+    };
+  },
+  methods: {
+    checkboxValues() {
+        if(this.checkedWorkout.includes('is_six_workout')) {
+            this.form.is_six_workout = "1"
+        }
+        if(this.checkedWorkout.includes('is_tabata_workout')) {
+            this.form.is_tabata_workout = "1"
+        }
+        if(this.checkedWorkout.includes('is_thirty_thirty_workout')) {
+            this.form.is_thirty_thirty_workout = "1"
+        }
+    },
+    trySubmit(e) {
+      e.preventDefault(e);
+      this.checkboxValues();
+      axios.post('exercices', this.form)
+      .then( res => res.data)
+      .then(alert(`L'exercice ${this.form.name} a bien été ajouté`))
+      .catch( err => console.log(err))
+    },
+  },
+};
+</script>
