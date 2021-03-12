32a5c44ee19ecb5e0a19b63e1e68f93aa2feef0c
Miura <tadamiura@gmail.com>
2021-03-12 14:36:36 +0100

    created a route to add exercice

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Admin from './components/Features/Admin/Admin'
+import AdminAddExercice from './components/Features/Admin/AdminAddExercice'
 import Profile from './components/Features/Profile/Profile'
 import SignIn from './components/Auth/SignIn'
 import SignUp from './components/Auth/SignUp'
@@ -11,30 +12,59 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
     mode: 'history',
-    routes: [
-        {path: '', redirect: '/exercice'},
-        {path: '/exercice', component: User},
-        {path: '/admin',
-        beforeEnter(to, from, next) {
-            if(store.getters["user/isLoggedIn"]) {
-                next()
-            } else {
-                router.push("/")
-            }
-        }, 
-        component: Admin},
-        {path: '/signin', component: SignIn},
-        {path: '/signup', component: SignUp},
-        {path: '/profile',
-        beforeEnter(to, from, next) {
-            if(store.getters["user/isLoggedIn"]) {
-                next()
-            } else {
-                router.push("/")
-            }
-        }, 
-        component: Profile},
-        {path: '**', redirect: '/exercice'}
+    routes: [{
+            path: '',
+            redirect: '/exercice'
+        },
+        {
+            path: '/exercice',
+            component: User
+        },
+        {
+            path: '/admin',
+            beforeEnter(to, from, next) {
+                if (store.getters["user/isLoggedIn"]) {
+                    next()
+                } else {
+                    router.push("/")
+                }
+            },
+            component: Admin
+        },
+        {
+            path: '/admin/exercices/new',
+            beforeEnter(to, from, next) {
+                if (store.getters["user/isLoggedIn"]) {
+                    next()
+                } else {
+                    router.push("/")
+                }
+            },
+            component: AdminAddExercice
+        },
+        {
+            path: '/signin',
+            component: SignIn
+        },
+        {
+            path: '/signup',
+            component: SignUp
+        },
+        {
+            path: '/profile',
+            beforeEnter(to, from, next) {
+                if (store.getters["user/isLoggedIn"]) {
+                    next()
+                } else {
+                    router.push("/")
+                }
+            },
+            component: Profile
+        },
+        {
+            path: '**',
+            redirect: '/exercice'
+        }
     ]
 })
 
